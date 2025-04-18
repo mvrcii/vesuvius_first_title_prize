69d7c4cf2a0e7701fc2a7bb48021e07b74b6b445
Micha Nowak <51212295+mvnowak@users.noreply.github.com>
2025-04-18 23:14:42 +0200

    update infer

--- infer_title.sh
+++ infer_title.sh
@@ -9,8 +9,11 @@ CHUNK_ID=2
 FRAGMENT_CHUNK_ID="$FRAGMENT_ID/parts_contrasted/${FRAGMENT_ID}_$(printf "%02d" $CHUNK_ID)"
 FRAGMENT_CHUNK_PATH="data/scroll5/fragments/${FRAGMENT_CHUNK_ID}"
 FRAGMENT_PATH="data/scroll5/fragments/$FRAGMENT_ID"
-CKPT_PATH="checkpoints/scroll5/winter-star-191-unetr-sf-b3-250414-063048-finetune/best-checkpoint-epoch=14.ckpt"
-START_LAYER_IDX=5
+CKPT_PATH="checkpoints/warm-planet-193-unetr-sf-b3-250417-171532/best-checkpoint-epoch=13-val_iou=0.71.ckpt"
+START_LAYER_IDX=0
+END_lAYER_IDX=5
+TTA_LEVEL=3
+STRIDE=4
 
 
 python scripts/download_fragments.py --fragment "$FRAGMENT_ID"
@@ -19,4 +22,4 @@ python scripts/fragment_splitter.py "$FRAGMENT_ID" -ch "$CHUNK_ID" --contrasted
 
 # Start inference
 echo "Starting inference on fragment $FRAGMENT_ID chunk $CHUNK_ID..."
-python scripts/inference.py "$FRAGMENT_CHUNK_PATH" "$CKPT_PATH" --start_layer "$START_LAYER_IDX"
+python scripts/inference.py "$FRAGMENT_CHUNK_PATH" "$CKPT_PATH" --start_layer "$START_LAYER_IDX" --end_layer "$START_LAYER_IDX" --tta "$TTA_LEVEL" --stride "$STRIDE" --title_crop
