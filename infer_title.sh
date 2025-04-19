e7743ef8906135fd07f7dd2210c5e2e71a990459
Micha Nowak <51212295+mvnowak@users.noreply.github.com>
2025-04-19 04:22:07 +0200

    Update infer_title.sh

--- infer_title.sh
+++ infer_title.sh
@@ -9,7 +9,7 @@ CHUNK_ID=2
 FRAGMENT_CHUNK_ID="$FRAGMENT_ID/parts_contrasted/${FRAGMENT_ID}_$(printf "%02d" $CHUNK_ID)"
 FRAGMENT_CHUNK_PATH="data/scroll5/fragments/${FRAGMENT_CHUNK_ID}"
 FRAGMENT_PATH="data/scroll5/fragments/$FRAGMENT_ID"
-CKPT_PATH="checkpoints/warm-planet-193-unetr-sf-b3-250417-171532/best-checkpoint-epoch=13-val_iou=0.71.ckpt"
+CKPT_PATH="checkpoints/scroll5/warm-planet-193-unetr-sf-b3-250417-171532/best-checkpoint-epoch=13-val_iou=0.71.ckpt"
 START_LAYER_IDX=3
 END_LAYER_IDX=5
 TTA_LEVEL=3
