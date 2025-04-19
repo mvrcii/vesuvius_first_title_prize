8d8f0c08c9bd04a8f67700d71b3528de088f64ac
mvnowak <micha.nowak98@gmx.de>
2025-04-19 03:57:19 +0200

    update infer hyperparams

--- infer_title.sh
+++ infer_title.sh
@@ -10,10 +10,10 @@ FRAGMENT_CHUNK_ID="$FRAGMENT_ID/parts_contrasted/${FRAGMENT_ID}_$(printf "%02d" 
 FRAGMENT_CHUNK_PATH="data/scroll5/fragments/${FRAGMENT_CHUNK_ID}"
 FRAGMENT_PATH="data/scroll5/fragments/$FRAGMENT_ID"
 CKPT_PATH="checkpoints/warm-planet-193-unetr-sf-b3-250417-171532/best-checkpoint-epoch=13-val_iou=0.71.ckpt"
-START_LAYER_IDX=0
-END_lAYER_IDX=5
+START_LAYER_IDX=3
+END_LAYER_IDX=5
 TTA_LEVEL=3
-STRIDE=4
+STRIDE=8
 
 
 python scripts/download_fragments.py --fragment "$FRAGMENT_ID"
@@ -22,4 +22,4 @@ python scripts/fragment_splitter.py "$FRAGMENT_ID" -ch "$CHUNK_ID" --contrasted
 
 # Start inference
 echo "Starting inference on fragment $FRAGMENT_ID chunk $CHUNK_ID..."
-python scripts/inference.py "$FRAGMENT_CHUNK_PATH" "$CKPT_PATH" --start_layer "$START_LAYER_IDX" --end_layer "$START_LAYER_IDX" --tta "$TTA_LEVEL" --stride "$STRIDE" --title_crop
+python scripts/inference.py "$FRAGMENT_CHUNK_PATH" "$CKPT_PATH" --start_layer "$START_LAYER_IDX" --end_layer "$END_LAYER_IDX" --tta "$TTA_LEVEL" --stride "$STRIDE" --title_crop
